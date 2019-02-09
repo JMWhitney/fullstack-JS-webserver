1b0f457a956183df3773a070b5d1bc1b9d0c8bbb
JMWhitney <JMWhitney96@gmail.com>
2019-02-08 23:39:17 -0800

    Working with MongoDB -All dynamic data is now read from MongoDB instead of the temporary JSON file. -Created an API to fetch a list of names -The list of names is now dynamically displayed on the front end

--- api/index.js
+++ api/index.js
@@ -15,26 +15,52 @@ MongoClient.connect(config.mongodbUri, { useNewUrlParser: true }, (err, client) 
 const router = express.Router();
 
 router.get('/contests', (req, res) => {
+
   let contests = {};
-  //Nodejs retrieves the data asynchronously 
-  db.collection('contests').find({})
-    .forEach((err, contest) => {
-      assert.equal(null, err);
-      console.log(contest);
-
-      
-      if(!contest) { //No more contests
-        res.send(contests);
-        return;
-      }
-
-      //Add each contest to contests array
+  //Query the database for all contests
+  db.collection('contests')
+  .find({})
+  .project({
+    //Return only id, categoryName, and contestName values
+    id: true,
+    categoryName: true,
+    contestName: true
+  })
+  .toArray((err, documents) => {
+    //Construct an object from the returned data and send it
+    assert.equal(null, err);
+    documents.forEach((contest) => {
       contests[contest.id] = contest;
-     });
+    })
+    //response must be sent here because nodejs queries the database asynchronously.
+    res.send({ contests });
+  });
+
+});
+
+router.get('/names/:nameIds', (req, res) => {
+  const nameIds = req.params.nameIds.split(',').map(Number);
+  let names = {};
+  //Query the database for all contests
+  db.collection('names')
+  .find({ id: {$in: nameIds }})
+  .toArray((err, documents) => {
+    //Construct an object from the returned data and send it
+    assert.equal(null, err);
+    documents.forEach((name) => {
+      names[name.id] = name;
+    })
+    //response must be sent here because nodejs queries the database asynchronously.
+    res.send({ names });
+  });
+
 });
 
 router.get('/contests/:contestId', (req, res) => {
-
+  db.collection('contests')
+  .findOne({ id: Number(req.params.contestId) })
+  .then(contest => res.send(contest))
+  .catch(console.error);
 });
 
 export default router;
